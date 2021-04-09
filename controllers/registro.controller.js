8cd859ca9c0e03bc31ffcc5b1d407693059e01f7
biorubenfs <biorubenfs@hotmail.com>
2021-04-09 20:46:20 +0200

    Agregado Registro Medicos en un nuevo endpoint

--- controllers/registro.controller.js
+++ controllers/registro.controller.js
@@ -1,3 +1,4 @@
+import Medico from "../models/medico.js";
 import Usuario from "../models/usuario.js";
 
 const controladorRegistro = {
@@ -14,6 +15,24 @@ const controladorRegistro = {
             await Usuario.create(nuevoUsuario);
 
             res.send(`Usuario registrado: ${nuevoUsuario.nombre}`);
+
+        } catch (error) {
+            res.status(400).send({ message: error.message });
+        }
+    },
+    crearMedico: async (req, res) => {
+
+        try {
+            const nombre = req.body.nombre;
+            const email = req.body.email;
+            const password = req.body.password;
+            const statusLog = 1;
+
+            const nuevoMedico = { nombre: nombre, email: email, password: password, statusLog: statusLog }
+
+            await Medico.create(nuevoMedico);
+
+            res.send(`Bienvenido Dr. ${nuevoMedico.nombre}`);
 
         } catch (error) {
             res.status(400).send({ message: error.message });
--- routes/registro.routes.js
+++ routes/registro.routes.js
@@ -3,6 +3,7 @@ import controladorRegistro from "../controllers/registro.controller.js";
 
 const rutaRegistro = Router();
 
-rutaRegistro.post('/', controladorRegistro.crearUsuario);
+rutaRegistro.post('/usuario', controladorRegistro.crearUsuario);
+rutaRegistro.post('/medico', controladorRegistro.crearMedico);
 
 export default rutaRegistro;
