e343dd9497f866e4871e108d096fed69e134a2b8
Galactuuus <galactuuus@gmail.com>
2021-04-11 12:06:58 +0200

    Middleware fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,13 @@ dotenv.config();
 
 const authMiddleware = (req, res, next) => {
     try{
+        /*
         jwt.verify(req.cookie.token, process.env.JWTOKEN);
+        next();
+        Lo recogemos del header puesto que no tenemos frontal 
+        y lo añadimos automaticamente en el postman;
+        */
+        jwt.verify(req.headers.token, process.env.JWTOKEN);
         next();
     }catch(e) { res.sendStatus(401) }
 }
